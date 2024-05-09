d7e0a0d1f899c443c9a1f41a59c6f867ee80871b
afnleaf <afnleafow@gmail.com>
2024-05-08 22:29:36 -0400

    Added: csv dictionary binding pokemon dex names to files in /pokemon_art

--- server/src/dev.ts
+++ server/src/dev.ts
@@ -1,17 +1,17 @@
 import { readdir } from 'node:fs/promises';
 import { join } from 'node:path';
-import {Dex} from '@pkmn/dex';
+import {Dex, Species} from '@pkmn/dex';
 
 // borrowed from some stack overflow page
-async function getFiles( directoryPath: string ) {
+async function getFiles(directoryPath: string) {
     try {
         // returns a JS array of just short/local file-names, not paths.
-        const fileNames = await readdir( directoryPath ); 
-        const filePaths = fileNames.map( fn => join( directoryPath, fn ) );
+        const fileNames = await readdir(directoryPath); 
+        const filePaths = fileNames.map(fn => join('', fn));
         return filePaths;
     }
-    catch (err) {
-        console.error( err );
+    catch(err) {
+        console.error(err);
     }
 }
 
@@ -19,17 +19,17 @@ async function getFiles( directoryPath: string ) {
 function naturalSort(a: string, b: string) {
     
     // replace "../pokemon_art/1024-t.png", to get "1024-t"
-    let a1: string = a.replace("../pokemon_art/", "");
-    let a2: string = a1.replace(".png", "");
-    let b1: string = b.replace("../pokemon_art/", "");
-    let b2: string = b1.replace(".png", "");
+    //let a1: string = a.replace("../pokemon_art/", "");
+    let a2: string = a.replace(".png", "");
+    //let b1: string = b.replace("../pokemon_art/", "");
+    let b2: string = b.replace(".png", "");
 
     // split into segments by -
     const posA = a2.indexOf("-");
     let segmentsA: string[] = [];
     if(posA !== -1) {
         segmentsA.push(a2.slice(0, posA));
-        segmentsA.push(a2.slice(posA));
+        segmentsA.push(a2.slice(posA+1));
     } else {
         segmentsA.push(a2);
         segmentsA.push("0");
@@ -38,7 +38,7 @@ function naturalSort(a: string, b: string) {
     let segmentsB: string[] = [];
     if(posB !== -1) {
         segmentsB.push(b2.slice(0, posB));
-        segmentsB.push(b2.slice(posB));
+        segmentsB.push(b2.slice(posB+1));
     } else {
         segmentsB.push(b2);
         segmentsB.push("0");
@@ -79,22 +79,36 @@ function naturalSort(a: string, b: string) {
 
 const directoryPath = "../pokemon_art";
 const filePaths = await getFiles(directoryPath);
-if(filePaths) {
-    filePaths.sort(naturalSort);
-}
-console.log(filePaths);
 
+//console.log(filePaths);
+
+const allPokemon: string[] = [];
+
+let i = 1;
 const mons = Dex.species.all();
 mons.forEach(pokemon => {
-    if(pokemon.isNonstandard === null) {
-        //console.log(pokemon);
-        console.log(pokemon.name);
-        const position = pokemon.name.indexOf("-");
-        if (position !== -1) {
-            const sliced = pokemon.name.slice(position);
-            console.log(`${pokemon.num}${sliced}.png`);
-        } else {
-            console.log(`${pokemon.num}.png`);
-        }
+    //allPokemon.push(pokemon.name);
+    //console.log(pokemon);
+    //console.log(pokemon.name);
+    allPokemon.push(pokemon.name);
+    /*
+    const position = pokemon.name.indexOf("-");
+    if (position !== -1) {
+        const sliced = pokemon.name.slice(position);
+        console.log(`${pokemon.num}${sliced}.png`);
+    } else {
+        console.log(`${pokemon.num}.png`);
     }
+    */
 });
+
+
+if(filePaths) {
+    filePaths.sort(naturalSort);
+    
+    for (let i = 0; i < Math.max(allPokemon.length, filePaths.length); i++) {
+        console.log(`${allPokemon[i]},${filePaths[i]}`);
+    }
+    
+    //console.log(filePaths);
+}
