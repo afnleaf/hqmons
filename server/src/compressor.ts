ba85ee35cce9ee53597741ebf61185782447d746
afnleaf <afnleafow@gmail.com>
2026-09-15 13:11:19 -0400

    items3

--- server/src/compressor.ts
+++ server/src/compressor.ts
@@ -1,7 +1,6 @@
 //compressor.ts
 
 // function to compress all the static files that need to be served
-// add 1 year cache headers
 async function compressor(filePath: string) {
     try {
         // read file
@@ -11,12 +10,13 @@ async function compressor(filePath: string) {
         // convert file to single unsigned int byte array and compress
         const compressed = Bun.gzipSync(new Uint8Array(await file.arrayBuffer()));
         // create response content with header
+        // add 1 year cache headers
         return new Response(compressed, {
             headers: {
                 "Content-Type": contentType ?? "application/octet-stream",
                 "Content-Encoding": "gzip",
                 "Cache-Control": "public, max-age=31536000, immutable",
-                "CDN-Cache-Control": "max-age=31536000",
+                "CDN-Cache-Control": "max-age=31536000, stale-if-error=31536000",
                 "Vary": "Accept-Encoding",
                 "ETag": `"${Buffer.from(compressed).toString('base64').substring(0, 27)}"`,
             },
